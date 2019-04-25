9801c947e6e4a5ef5444c1a5a636fa95b5b6d8b4
Jakob Unterwurzacher <jakobunt@gmail.com>
2019-04-25 21:29:30 +0200

    Add test_blank_btrfs.sh convenience helper
    $ ./test_blank_btrfs.sh
    Linux brikett 5.0.4-200.fc29.x86_64 #1 SMP Mon Mar 25 02:27:33 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux
    mounted at /tmp/tmp.rJiybJjQsm.mnt
    reading from /dev/urandom
    writing to ./blob.LXYsCY
    writing blocks of 132096 bytes each
    total    125 MiB, 162.76 MiB/s
    total    251 MiB, 160.59 MiB/s
    total    377 MiB, 161.60 MiB/s
    total    503 MiB, 111.68 MiB/s
    total    629 MiB, 164.14 MiB/s
    total    755 MiB, 129.62 MiB/s
    pwrite failed: No space left on device

---
+++ test_blank_btrfs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+uname -a
+IMG=$(mktemp)
+MNT=$IMG.mnt
+mkdir $MNT
+truncate -s 1G $IMG
+mkfs.btrfs -q $IMG
+sudo mount $IMG $MNT
+echo "mounted at $MNT"
+sudo chmod 777 $MNT
+MYDIR=$PWD
+cd $MNT
+$MYDIR/fallocate_write || true
+cd $MYDIR
+sudo umount $MNT
+rm $IMG
