39c373e9ea477b25cd5b6f7072de4b00d42de522
Jakob Unterwurzacher <jakobunt@gmail.com>
2019-04-27 13:10:10 +0200

    test_blank_btrfs: print what is being done
    So people see what happens without looking at the source code

--- test_blank_btrfs.sh
+++ test_blank_btrfs.sh
@@ -6,14 +6,16 @@ uname -a
 IMG=$(mktemp)
 MNT=$IMG.mnt
 mkdir $MNT
+echo "allocating 1GB at $IMG, formatting as btrfs"
 fallocate -l 1G $IMG
 mkfs.btrfs -q $IMG
+echo "mounting to $MNT"
 sudo mount $IMG $MNT
-echo "mounted at $MNT"
 sudo chmod 777 $MNT
 MYDIR=$PWD
 cd $MNT
 $MYDIR/fallocate_write || true
+echo "cleaning up"
 cd $MYDIR
 sudo umount $MNT
 rm $IMG
